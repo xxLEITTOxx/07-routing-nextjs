11988504c73205a555c574894b08ec73b84ac81a
LEITTO <xxleittoxx@gmail.com>
2025-10-05 23:20:28 +0200

    modal window added

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,15 +22,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>
         <TanStackProvider>
           <Header />
-          <main>{children}</main>
+          <main>
+            {children}
+            {modal}
+          </main>
           <Footer />
         </TanStackProvider>
       </body>
